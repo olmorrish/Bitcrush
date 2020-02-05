0aa93b3de192e5a43653329e4519d8abd6cbb60a
Oliver Morrish <olmorris@ucalgary.ca>
2020-02-05 16:17:59 -0700

    Updated Build Path for Scenes
    Also removed some extranous files from old build. Game is currently in a playable state.

--- Assets/Scripts/MenuScripts/MainMenu.cs
+++ Assets/Scripts/MenuScripts/MainMenu.cs
@@ -4,19 +4,9 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 	
 	public void StartGame(){
-		SceneManager.LoadScene("Game", LoadSceneMode.Single);
+		SceneManager.LoadScene("NormalMode", LoadSceneMode.Single);
 	}
 	
 	public void StartTrominoGame(){
