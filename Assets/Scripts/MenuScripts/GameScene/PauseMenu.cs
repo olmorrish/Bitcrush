6b3dfc32231e28c4a1bfdc42cb0d609d220cf826
Oliver Morrish <olmorris@ucalgary.ca>
2020-02-09 12:22:37 -0700

    Added Arrow Key Controls

--- Assets/Scripts/MenuScripts/GameScene/PauseMenu.cs
+++ Assets/Scripts/MenuScripts/GameScene/PauseMenu.cs
@@ -17,7 +17,7 @@ public class PauseMenu : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonDown("Start")){
+		if(Input.GetButtonDown("Start")){
 			if(isPaused){
 				Resume();
 			}
