346441d875046be86478ca781a05446bc3a71801
Oliver Morrish <olmorris@ucalgary.ca>
2020-02-09 09:46:25 -0700

    Complete Main Menu Overhaul
    Added GameModes and CustomGame Menus. Delegated game launch resbonsibilities to a separate MenuNavigator GameObject.

--- Assets/Scripts/MenuScripts/MainMenu.cs
+++ Assets/Scripts/MenuScripts/MainMenu.cs
@@ -2,16 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
 
     public GameObject settingsObject;
     private PersistentSettings settings;
 
+    public GameObject mainMenuCanvas;
+    public Button mainMenuDefaultButton;
+    public GameObject gameModesCanvas;
+    public Button gameModesDefaultButton;
+    public GameObject customGamesCanvas;
+    public Button customGamesDefaultButton;
+
     private void Start() {
         settings = settingsObject.GetComponent<PersistentSettings>();
+        mainMenuCanvas.SetActive(true);
+        gameModesCanvas.SetActive(false);
+        customGamesCanvas.SetActive(false);
+        mainMenuDefaultButton.Select();
     }
 
+    ///////////////////
+    /// Menu Navigation
+    ///////////////////
+
+    public void ToMainMenuCanvas() {
+        Debug.Log("Going to main menu.");
+        mainMenuCanvas.SetActive(true);
+        gameModesCanvas.SetActive(false);
+        customGamesCanvas.SetActive(false);
+        mainMenuDefaultButton.Select();
+    }
+
+    public void ToGameModeCanvas() {
+        Debug.Log("Going to game modes menu.");
+        gameModesCanvas.SetActive(true);
+        mainMenuCanvas.SetActive(false);
+        customGamesCanvas.SetActive(false);
+        gameModesDefaultButton.Select();
+    }
+
+    public void ToCustomGamesCanvas() {
+        Debug.Log("Going to custom games menu.");
+        mainMenuCanvas.SetActive(false);
+        gameModesCanvas.SetActive(false);
+        customGamesCanvas.SetActive(true);
+        customGamesDefaultButton.Select();
+    }
+
+    ///////////////////
+    /// Mode Launchers
+    ///////////////////
     public void StartGame(){
         //apply the relevant settings to store in the persistent object - prior to loading
         SetDefaultSettings();
@@ -33,8 +76,16 @@ public class MainMenu : MonoBehaviour {
         settings.fireMode = "pentomino";
         LoadGame();
     }
-	
-	public void QuitGame(){
+
+    public void StartBiTCHRUSHERGame() {
+        SetDefaultSettings();   //default - overwrite any others below
+        settings.fireMode = "all";
+        settings.minWaitTime = 0.2f;
+        settings.maxWaitTime = 1.75f;
+        LoadGame();
+    }
+
+        public void QuitGame(){
 		Application.Quit();
 	}
 
