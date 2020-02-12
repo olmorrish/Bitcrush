274ac9b69b5fffe707d6d79d20df863a055c36d5
Oliver Morrish <olmorris@ucalgary.ca>
2020-02-12 14:25:04 -0700

    Blocks Now "Dull" in Hue When Frozen

--- Assets/Scripts/Block and KillLine/Freezer.cs
+++ Assets/Scripts/Block and KillLine/Freezer.cs
@@ -6,20 +6,25 @@ public class Freezer : MonoBehaviour {
 
 	public bool frozen; //used to check when player collides - player explodes if not frozen
 
-	private Rigidbody2D rb;
     public AudioSource[] hitFX = new AudioSource[2];
+    private Rigidbody2D blockRB;
+
+    private SpriteRenderer blockRend;
+    [Range(0, 1)] public float dullHueTolerance= 0.52f;
+    [Range(0, 1)] public float dullPercentageOnFreeze = 0.4f;
 
     void Awake () {
         frozen = false;
 
-		rb = GetComponent<Rigidbody2D>();
+		blockRB = GetComponent<Rigidbody2D>();
+        blockRend = GetComponent<SpriteRenderer>();
         hitFX[0] = GameObject.Find("HitFX1").GetComponent<AudioSource>();
         hitFX[1] = GameObject.Find("HitFX2").GetComponent<AudioSource>();
     }
 	
 	void Update(){
         //if not moving, freeze
-		if(rb.velocity.magnitude < 0.01f && !frozen){
+		if(blockRB.velocity.magnitude < 0.01f && !frozen){
 			Freeze();
 		}
 	}
@@ -29,7 +34,32 @@ public class Freezer : MonoBehaviour {
         frozen = true;
 
         hitFX[Random.Range(0, hitFX.Length - 1)].Play();
-		rb.constraints = RigidbodyConstraints2D.FreezeAll;
-		rb.velocity = Vector3.zero;
-	}	
+		blockRB.constraints = RigidbodyConstraints2D.FreezeAll;
+		blockRB.velocity = Vector3.zero;
+
+        blockRend.color = GreyOut(blockRend.color, dullHueTolerance, dullPercentageOnFreeze);    //reduces hues within 52% of the highest hue to 40% of their value
+    }	
+
+    /*
+     * tolerance is how near a hue must be to the highest RGB hue to be reduced (from 0 to 1)
+     * dullStrength is the percentage by which relevant hues will be reduced
+     */
+    private Color GreyOut(Color c, float tolerance, float dullStrength) {
+
+        float[] hueValues = new float[3];   //colour mod is done in-place in this array
+        hueValues[0] = c.r;
+        hueValues[1] = c.g;
+        hueValues[2] = c.b;
+
+        float maxHue = Mathf.Max(hueValues);
+
+        for (int i = 0; i < 3; i++) {
+            float offsetFromMaxHue = Mathf.Abs(maxHue - hueValues[i]);
+            if(offsetFromMaxHue <= tolerance) {
+                hueValues[i] = (hueValues[i] * (1-dullStrength));     //if the hue is within a certain range of the highest hue, dull it
+            }
+        }
+
+        return new Color(hueValues[0], hueValues[1], hueValues[2]);
+    }
 }
