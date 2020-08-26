770b9a2414fc638f1b5fc901a490be441245e173
Oliver Morrish <olmorris@ucalgary.ca>
2020-08-26 14:01:44 -0600

    Highscores Update on Menu Start

---
+++ Assets/UpdateHighscore.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateHighscore : MonoBehaviour {
+
+    public string prefsHighscoreVarName;
+
+    // Start is called before the first frame update
+    void Start() {
+
+        gameObject.GetComponent<Text>().text = PlayerPrefs.GetInt(prefsHighscoreVarName, 0).ToString();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
