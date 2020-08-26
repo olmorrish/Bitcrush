b9e679003a846710ecb2b828b6ab58f7af15d05f
Oliver Morrish <olmorris@ucalgary.ca>
2020-08-26 15:16:30 -0600

    Added Disabling of Mode Names for Locked Modes

--- Assets/PlayerProgress.cs
+++ Assets/PlayerProgress.cs
@@ -1,35 +1,106 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+/* PlayerProgress
+ * This script deals with any unlocks or changes made the the menu based on PlayerPrefs variables.
+ * 
+ */
 public class PlayerProgress : MonoBehaviour {
 
-    [Header ("HighScores")]
-    public int HS_Normal;
-    public int HS_Casual;
-    public int HS_Tromino;
-    public int HS_Pentomino;
-    public int HS_Tiny;
-    public int HS_Bitcrusher;
-    public int HS_SlipperySlopes;
-    public int HS_UpsideDown;
-    public int HS_UniversitySim;
-    public int HS_Bitwarped;
-    public int HS_Bitcrusher2;
+    [Header("GameMode Menu Buttons")]
+    public Button casualButton;
+    public Button trominoButton;
+    public Button pentominoButton;
+    public Button tinyButton;
+    public Button bitcrusherButton;
+    public Button slipperySlopesButton;
+    public Button upsideDownButton;
+    public Button universitySimButton;
+    public Button bitwarpedButton;
+    public Button bitcrusher2Button;
+    public Button impossibleButton;
+
+    [Header("Highscore Menu Text Objs")]
+    public GameObject casualHSText;
+    public GameObject trominoHSText;
+    public GameObject pentominoHSText;
+    public GameObject tinyHSText;
+    public GameObject bitcrusherHSText;
+    public GameObject slipperySlopesHSText;
+    public GameObject upsideDownHSText;
+    public GameObject universitySimHSText;
+    public GameObject bitwarpedHSText;
+    public GameObject bitcrusher2HSText;
+    //public GameObject impossibleHSText;
 
     // Start is called before the first frame update
     void Start() {
-        HS_Normal = PlayerPrefs.GetInt("HS_Normal", 0);
-        HS_Casual = PlayerPrefs.GetInt("HS_Casual", 0);
-        HS_Tromino = PlayerPrefs.GetInt("HS_Tromino", 0);
-        HS_Pentomino = PlayerPrefs.GetInt("HS_Pentomino", 0);
-        HS_Tiny = PlayerPrefs.GetInt("HS_Tiny", 0);
-        HS_Bitcrusher = PlayerPrefs.GetInt("HS_Bitcrusher", 0);
-        HS_SlipperySlopes = PlayerPrefs.GetInt("HS_SlipperySlopes", 0);
-        HS_UpsideDown = PlayerPrefs.GetInt("HS_UpsideDown", 0);
-        HS_UniversitySim = PlayerPrefs.GetInt("HS_UniversitySim", 0);
-        HS_Bitwarped = PlayerPrefs.GetInt("HS_Bitwarped", 0);
-        HS_Bitcrusher2 = PlayerPrefs.GetInt("HS_Bitcrusher2", 0);
+        if(PlayerPrefs.GetInt("UL_Casual", 0) == 0) {
+            casualButton.interactable = false;
+            casualButton.GetComponentInChildren<Text>().text = "???";
+            casualHSText.GetComponent<Text>().text = "???";
+        }
+
+        if (PlayerPrefs.GetInt("UL_Tromino", 0) == 0) {
+            trominoButton.interactable = false;
+            trominoButton.GetComponentInChildren<Text>().text = "???";
+            trominoHSText.GetComponent<Text>().text = "???";
+        }
+
+        if (PlayerPrefs.GetInt("UL_Pentomino", 0) == 0) {
+            pentominoButton.interactable = false;
+            pentominoButton.GetComponentInChildren<Text>().text = "???";
+            pentominoHSText.GetComponent<Text>().text = "???";
+        }
+
+        if (PlayerPrefs.GetInt("UL_Tiny", 0) == 0) {
+            tinyButton.interactable = false;
+            tinyButton.GetComponentInChildren<Text>().text = "???";
+            tinyHSText.GetComponent<Text>().text = "???";
+        }
+
+        if (PlayerPrefs.GetInt("UL_Bitcrusher", 0) == 0) {
+            bitcrusherButton.interactable = false;
+            bitcrusherButton.GetComponentInChildren<Text>().text = "???";
+            bitcrusherHSText.GetComponent<Text>().text = "???";
+        }
+
+        if (PlayerPrefs.GetInt("UL_SlipperySlopes", 0) == 0) {
+            slipperySlopesButton.interactable = false;
+            slipperySlopesButton.GetComponentInChildren<Text>().text = "???";
+            slipperySlopesHSText.GetComponent<Text>().text = "???";
+        }
+
+        if (PlayerPrefs.GetInt("UL_UpsideDown", 0) == 0) {
+            upsideDownButton.interactable = false;
+            upsideDownButton.GetComponentInChildren<Text>().text = "???";
+            upsideDownHSText.GetComponent<Text>().text = "???";
+        }
+
+        if (PlayerPrefs.GetInt("UL_UniversitySim", 0) == 0) {
+            universitySimButton.interactable = false;
+            universitySimButton.GetComponentInChildren<Text>().text = "???";
+            universitySimHSText.GetComponent<Text>().text = "???";
+        }
+
+        if (PlayerPrefs.GetInt("UL_Bitwarped", 0) == 0) {
+            bitwarpedButton.interactable = false;
+            bitwarpedButton.GetComponentInChildren<Text>().text = "???";
+            bitwarpedHSText.GetComponent<Text>().text = "???";
+        }
+
+        if (PlayerPrefs.GetInt("UL_Bitcrusher2", 0) == 0) {
+            bitcrusher2Button.interactable = false;
+            bitcrusher2Button.GetComponentInChildren<Text>().text = "???";
+            bitcrusher2HSText.GetComponent<Text>().text = "???";
+        }
+
+        if (PlayerPrefs.GetInt("UL_Impossible", 0) == 0) {
+            impossibleButton.interactable = false;
+            impossibleButton.GetComponentInChildren<Text>().text = "???";
+        }
     }
 
     // Update is called once per frame
--- Assets/UpdateHighscore.cs
+++ Assets/UpdateHighscore.cs
@@ -9,9 +9,7 @@ public class UpdateHighscore : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
-
-        gameObject.GetComponent<Text>().text = PlayerPrefs.GetInt(prefsHighscoreVarName, 0).ToString();
-
+        gameObject.GetComponent<Text>().text = PlayerPrefs.GetInt(prefsHighscoreVarName, 0).ToString(); //all highscores default to 0
     }
 
     // Update is called once per frame
