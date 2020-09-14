c5d96e8e16cef1e023a51ce9fdd2913fc8a3b588
Oliver Morrish <olmorris@ucalgary.ca>
2020-09-14 17:08:52 -0600

    Upside-Down Highscore is Now Negative

--- Assets/Scripts/UpdateHighscore.cs
+++ Assets/Scripts/UpdateHighscore.cs
@@ -5,11 +5,19 @@ using UnityEngine.UI;
 
 public class UpdateHighscore : MonoBehaviour {
 
+    public bool addNegativeSign = false;
     public string prefsHighscoreVarName;
 
     // Start is called before the first frame update
     void Start() {
-        gameObject.GetComponent<Text>().text = PlayerPrefs.GetInt(prefsHighscoreVarName, 0).ToString(); //all highscores default to 0
+
+        Text scoreText = gameObject.GetComponent<Text>();
+        int currentHighscore = PlayerPrefs.GetInt(prefsHighscoreVarName, 0);
+        scoreText.text = currentHighscore.ToString(); //all highscores default to 0
+
+        if (addNegativeSign && currentHighscore > 0) {
+            scoreText.text = "-" + scoreText.text;
+        }
     }
 
     // Update is called once per frame
