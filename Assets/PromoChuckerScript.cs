da9fed2fe62b6d323a3c7f63b36cd2e7c5b6743c
BuildTools <olmorris@ucalgary.ca>
2021-06-03 17:00:35 -0600

    Added a Promo Scene
    Added a scene using the chucker objects to make an animated background for the itch icon.

---
+++ Assets/PromoChuckerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PromoChuckerScript : MonoBehaviour {
+
+    public ChuckerBehavior[] chuckers;
+    float nextChuckTime = Time.time;
+    public float timeBetweenChucks;
+    int chuckerIndex = 0;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+        if(Time.time > nextChuckTime) {
+            chuckers[chuckerIndex].Throw("tetromino", false, new BlockPalette("default"));
+            chuckerIndex = (chuckerIndex + 1) % chuckers.Length;
+            nextChuckTime = Time.time + timeBetweenChucks;
+        }
+
+    }
+}
